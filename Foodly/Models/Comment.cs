d6361d25fa4fd84fabbaec66016452838327d3b6
Meeyzt <mehmetbs@hotmail.com.tr>
2020-11-21 17:49:49 +0300

    Models

---
+++ Foodly/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodly.Models
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Entry { get; set; }
+        public int Like { get; set; }
+        public int Dislike { get; set; }
+
+    }
+}
---
+++ Foodly/Models/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodly.Models
+{
+    public class RestaurantModel
+    {
+        public int ID { get; set; }
+        public string RestaurantName { get; set; }
+        public Double StarCount { get; set; }
+        public string FavProduct { get; set; }
+    }
+}
