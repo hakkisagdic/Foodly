528008c67b3f2073b8412f45a86d05aa63db8a8e
Meeyzt <mehmetbs@hotmail.com.tr>
2020-11-25 15:23:22 +0300

    Updating .gitignore

--- Foodly/Models/ErrorViewModel.cs
+++ Foodly/Models/ErrorViewModel.cs
@@ -1,9 +1,13 @@
 using System;
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 namespace Foodly.Models
 =======
 namespace Foodly.Models
+>>>>>>> Stashed changes
+=======
+namespace WebApplication1.Models
 >>>>>>> Stashed changes
 {
     public class ErrorViewModel
