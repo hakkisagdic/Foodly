19b2d7415345f1a7e6ac93a540a165050b34525e
Meeyzt <mehmetbs@hotmail.com.tr>
2020-11-25 15:02:45 +0300

    Create-Migration

---
+++ Foodly/Migrations/20201125103913_Migration1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Foodly.Migrations
+{
+    public partial class Migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(nullable: false),
+                    Password = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    DisplayName = table.Column<string>(nullable: false),
+                    RegisterDate = table.Column<DateTime>(nullable: false),
+                    SecretKey = table.Column<string>(nullable: false),
+                    Auth = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
